f5ae667e0bc633ac964a5c8a8e7ab4f376a0e75b
Filip Rembiałkowski <filip.rembialkowski@onebi.eu>
2016-02-15 18:19:10 +0100

    notify all

--- contrib/tcn/tcn.c
+++ contrib/tcn/tcn.c
@@ -160,7 +160,7 @@ triggered_change_notification(PG_FUNCTION_ARGS)
 					strcpy_quoted(payload, SPI_getvalue(trigtuple, tupdesc, colno), '\'');
 				}
 
-				Async_Notify(channel, payload->data);
+				Async_Notify(channel, payload->data, false);
 			}
 			ReleaseSysCache(indexTuple);
 			break;
